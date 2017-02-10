00e8c4b503a9301d723906e8e07f0204eb16c05c
Vijay Radha <polymorphic.technologies@gmail.com>
2017-02-10 21:24:59 +0000

    Initial commit

---
+++ src/main/java/uk/co/codingtest/bg/BerlinClockApplication.java
@@ -0,0 +1,12 @@
+package uk.co.codingtest.bg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BerlinClockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BerlinClockApplication.class, args);
+	}
+}
